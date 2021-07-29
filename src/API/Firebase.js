203c799d98206bfdd720d938c5433bf4fd3724b2
kouskenemy <kosukenemy@gmail.com>
2021-07-30 00:26:43 +0900

    firebase configにdatabaseURL追記

--- src/API/Firebase.js
+++ src/API/Firebase.js
@@ -6,12 +6,11 @@ import 'firebase/firestore';
 var firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
     authDomain:process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_URL,
+    databaseURL: `https://${process.env.REACT_APP_FIREBASE_ID}.firebaseio.com`,
     projectId: process.env.REACT_APP_FIREBASE_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_SB,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MSI,
     appId: process.env.REACT_APP_FIREBASE_APPID,
-    databaseURL: `https://${process.env.REACT_APP_FIREBASE_ID}.firebaseio.com`,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
